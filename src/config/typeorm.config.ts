c291f44ea5ef0791968995e922b854c72437a1e5
huybd-hblab <huybd@hblab.vn>
2025-12-28 14:55:10 +0700

    update migration step github actions

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -10,10 +10,13 @@ export const dataSourceOptions: DataSourceOptions = {
   username: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
-  entities: ['src/**/*.entity.ts'], // Use TS entities for migrations via ts-node
-  migrations: ['src/migrations/*.ts'],
+  entities: [__dirname + '/../**/*.entity.{js,ts}'],
+  migrations: [__dirname + '/../migrations/*.{js,ts}'],
   synchronize: false, // Migrations should handle schema changes
-  ssl: process.env.DATABASE_SSL === 'true' ? { rejectUnauthorized: false } : undefined,
+  ssl:
+    process.env.DATABASE_SSL === 'true'
+      ? { rejectUnauthorized: false }
+      : undefined,
 };
 
 const dataSource = new DataSource(dataSourceOptions);
